3aee027567d2a2a7d79e51e5e942996af3f09ec2
naohiro kosuda <kosnago0428@gmail.com>
2025-12-15 22:27:45 +0900

    Implement tilde expansion for paths

--- src/expand.rs
+++ src/expand.rs
@@ -1,5 +1,7 @@
 use std::path::{Path, PathBuf};
 
+use crate::path_utils;
+
 pub trait HomeDir {
     fn expand_home(&self) -> PathBuf {
         todo!()
@@ -8,7 +10,17 @@ pub trait HomeDir {
 
 impl HomeDir for Path {
     fn expand_home(&self) -> PathBuf {
-        println!("{:?}", self);
-        todo!()
+        match self.to_str() {
+            Some(s) => {
+                if s == "~" {
+                    path_utils::home_dir()
+                } else {
+                    s.strip_prefix("~/")
+                        .map(|rest| path_utils::home_dir().join(rest))
+                        .unwrap_or_else(|| self.to_path_buf())
+                }
+            }
+            None => self.to_path_buf(),
+        }
     }
 }
