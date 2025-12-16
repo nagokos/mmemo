653f6531002df28f005136e7d83586b1895ab0b1
naohiro kosuda <kosnago0428@gmail.com>
2025-12-16 21:52:31 +0900

    add utils

--- src/app/path_utils.rs
+++ src/app/path_utils.rs
@@ -5,3 +5,19 @@ pub fn home_dir() -> PathBuf {
         .map(PathBuf::from)
         .expect("HOME environment variable not set")
 }
+
+pub fn config_dir() -> PathBuf {
+    home_dir().join(".config").join("mmemo")
+}
+
+pub fn mmemo_dir() -> PathBuf {
+    home_dir().join("mmemo")
+}
+
+pub fn config_path() -> PathBuf {
+    config_dir().join("config.toml")
+}
+
+pub fn template_path() -> PathBuf {
+    config_dir().join("template.md")
+}
