5427530a5e8200d35e678b8feb010df869b0f6db
naohiro kosuda <kosnago0428@gmail.com>
2025-12-20 15:14:56 +0900

    add fn expand_home

--- src/app/expand.rs
+++ src/app/expand.rs
@@ -1,26 +1,26 @@
 use std::path::{Path, PathBuf};
 
-use crate::app::path_utils;
+use crate::app::{error::MmemoResult, path_utils};
 
 pub trait HomeDir {
-    fn expand_home(&self) -> PathBuf {
-        todo!()
-    }
+    fn expand_home(&self) -> MmemoResult<PathBuf>;
 }
 
 impl HomeDir for Path {
-    fn expand_home(&self) -> PathBuf {
+    fn expand_home(&self) -> MmemoResult<PathBuf> {
         match self.to_str() {
             Some(s) => {
                 if s == "~" {
-                    path_utils::home_dir()
+                    Ok(path_utils::home_dir()?)
                 } else {
-                    s.strip_prefix("~/")
-                        .map(|rest| path_utils::home_dir().join(rest))
-                        .unwrap_or_else(|| self.to_path_buf())
+                    let path = s
+                        .strip_prefix("~/")
+                        .and_then(|rest| path_utils::home_dir().ok().map(|path| path.join(rest)))
+                        .unwrap_or_else(|| self.to_path_buf());
+                    Ok(path)
                 }
             }
-            None => self.to_path_buf(),
+            None => Ok(self.to_path_buf()),
         }
     }
 }
