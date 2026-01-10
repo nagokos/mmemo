956221f222ade86aa366e4d5d4fa635a297be3e5
nagokos <kosnago0428@gmail.com>
2026-01-11 04:21:11 +0900

    refactor(cli): Simplify the parser design.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fmt::Display, str::FromStr};
+use std::env::args;
 
 use crate::app::run;
 
@@ -8,17 +8,11 @@ mod app;
 enum CliParseError {
     // TODO: cr helloとかでParse error: helloになるのはおかしいので考える
     // helpと同じような感じでもいいのかも
-    #[error("Parse error: {command}")]
+    #[error("unknown command: mmemo {command}")]
     UnknownCommand { command: String },
 
-    #[error("command not found")]
-    CommandNotFound,
-
-    #[error("Usage: {usage}")]
+    #[error("usage: {usage}")]
     MissingArgument { usage: String },
-
-    #[error("Usage: {usage}")]
-    TooManyArguments { usage: String },
 }
 
 #[derive(Debug)]
@@ -28,105 +22,49 @@ enum Command {
     Edit,
     Delete,
     List,
-    Grep(Vec<String>, String),
+    Grep(Vec<String>),
     View,
     Config,
     Help,
     Version,
 }
 
-impl Command {
-    fn parse(args: Vec<String>) -> Result<Command, CliParseError> {
+impl TryFrom<Vec<String>> for Command {
+    type Error = CliParseError;
+
+    fn try_from(args: Vec<String>) -> Result<Self, Self::Error> {
         match args.first() {
-            Some(cmd) => {
-                let cmd: Command = cmd.parse().map_err(|_| CliParseError::UnknownCommand {
-                    command: cmd.to_string(),
-                })?;
-
-                match cmd {
-                    Command::Init => Ok(Command::Init),
-                    Command::New(_) => {
-                        let title: Vec<String> = args.into_iter().skip(1).collect();
-                        if title.is_empty() {
-                            return Err(CliParseError::MissingArgument {
-                                usage: "mmemo new <title>".to_string(),
-                            });
-                        }
-                        Ok(Command::New(title.join(" ")))
+            Some(s) => match s.as_str() {
+                "init" | "i" => Ok(Command::Init),
+                "new" | "n" => {
+                    let title: Vec<String> = args.into_iter().skip(1).collect();
+                    if title.is_empty() {
+                        return Err(CliParseError::MissingArgument {
+                            usage: "mmemo new <title>".to_string(),
+                        });
                     }
-                    Command::Edit => Ok(Command::Edit),
-                    Command::Delete => Ok(Command::Delete),
-                    Command::List => Ok(Command::List),
-                    Command::View => Ok(Command::View),
-                    Command::Grep(_, _) => {
-                        let mut options = Vec::new();
-                        let mut target = String::new();
-
-                        for arg in args.into_iter().skip(1) {
-                            if arg.starts_with("-") {
-                                options.push(arg);
-                            } else if target.is_empty() {
-                                target = arg;
-                            } else {
-                                return Err(CliParseError::TooManyArguments {
-                                    usage: "mmemo grep <pattern>".to_string(),
-                                });
-                            }
-                        }
-
-                        if target.is_empty() {
-                            return Err(CliParseError::MissingArgument {
-                                usage: "mmemo grep <pattern>".to_string(),
-                            });
-                        }
-                        Ok(Command::Grep(options, target))
-                    }
-                    Command::Config => Ok(Command::Config),
-                    Command::Help => Ok(Command::Help),
-                    Command::Version => Ok(Command::Version),
+                    Ok(Command::New(title.join(" ")))
                 }
-            }
-            None => Err(CliParseError::CommandNotFound),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct CommandParseError;
-
-impl FromStr for Command {
-    type Err = CommandParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "init" | "i" => Ok(Command::Init),
-            "new" | "n" => Ok(Command::New(String::new())),
-            "edit" | "e" => Ok(Command::Edit),
-            "delete" | "d" => Ok(Command::Delete),
-            "list" | "l" => Ok(Command::List),
-            "grep" | "g" => Ok(Command::Grep(Vec::new(), String::new())),
-            "view" | "v" => Ok(Command::View),
-            "config" | "c" => Ok(Command::Config),
-            "help" | "-h" | "--help" => Ok(Command::Help),
-            "version" | "-v" | "--version" => Ok(Command::Version),
-            _ => Err(CommandParseError),
-        }
-    }
-}
-
-impl Display for Command {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Command::Init => write!(f, "init"),
-            Command::New(s) => write!(f, "new {}", s),
-            Command::Edit => write!(f, "edit"),
-            Command::Delete => write!(f, "delete"),
-            Command::List => write!(f, "list"),
-            Command::Grep(o, s) => write!(f, "grep {} {}", o.join(" "), s),
-            Command::View => write!(f, "view"),
-            Command::Config => write!(f, "config"),
-            Command::Help => write!(f, "help"),
-            Command::Version => write!(f, "version"),
+                "edit" | "e" => Ok(Command::Edit),
+                "delete" | "d" => Ok(Command::Delete),
+                "list" | "l" => Ok(Command::List),
+                "grep" | "g" => {
+                    let rest: Vec<String> = args.into_iter().skip(1).collect();
+                    if rest.is_empty() {
+                        return Err(CliParseError::MissingArgument {
+                            usage: "mmemo grep <pattern...>".to_string(),
+                        });
+                    }
+                    Ok(Command::Grep(rest))
+                }
+                "view" | "v" => Ok(Command::View),
+                "config" | "c" => Ok(Command::Config),
+                // TODO: commandとして扱わないでここでやるとか
+                "help" | "-h" | "--help" => Ok(Command::Help),
+                "version" | "-v" | "--version" => Ok(Command::Version),
+                _ => Err(CliParseError::UnknownCommand { command: s.into() }),
+            },
+            None => Ok(Command::Help),
         }
     }
 }
@@ -134,7 +72,7 @@ impl Display for Command {
 fn main() {
     let args: Vec<String> = args().skip(1).collect();
 
-    let cmd = Command::parse(args).unwrap_or_else(|e| {
+    let cmd: Command = args.try_into().unwrap_or_else(|e| {
         eprintln!("{e}");
         std::process::exit(1);
     });
